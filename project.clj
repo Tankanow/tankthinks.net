21241ab53c2f3ffd22388e72ffef676db01009bb
Adam Tankanow <atankanow@massmutual.com>
2016-12-09 15:45:34 -0500

    update deps to latest versions

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
             :url "https://github.com/lacarmen/cryogen"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :dependencies [[org.clojure/clojure "1.7.0"]
-                           [ring/ring-devel "1.4.0"]
-                           [compojure "1.4.0"]
+            :dependencies [[org.clojure/clojure "1.8.0"]
+                           [ring/ring-devel "1.5.0"]
+                           [compojure "1.5.1"]
                            [ring-server "0.4.0"]
                            [cryogen-asciidoc "0.1.2"]
-                           [cryogen-core "0.1.26"]]
-            :plugins [[lein-ring "0.9.6"]]
+                           [cryogen-core "0.1.43"]]
+            :plugins [[lein-ring "0.10.0"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
                    :handler cryogen.server/handler})
